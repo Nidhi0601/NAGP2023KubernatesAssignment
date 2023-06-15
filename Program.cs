683103996a71e3889442fee701880d22730f68e4
Nidhi0601 <136242855+Nidhi0601@users.noreply.github.com>
2023-06-15 20:48:34 +0530

    Update Program.cs (#1)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using NAGP2023KubernatesAssignment.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+var base64Bytes = Convert.FromBase64String(builder.Configuration["db-password"]);
+var password = System.Text.Encoding.UTF8.GetString(base64Bytes);
 
 // Add services to the container.
-builder.Services.AddDbContext<CoreDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+SqlConnection conn = new SqlConnection(new SqlConnectionStringBuilder() { DataSource = builder.Configuration["db-host"], InitialCatalog = builder.Configuration["db-name"], 
+    UserID = builder.Configuration["db-user"], Password = password}.ConnectionString);
+builder.Services.AddDbContext<CoreDbContext>(op => op.UseSqlServer(conn));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
